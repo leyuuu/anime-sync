61b6e288a08ab73c025a4e32732dcfcc93e29a53
Kyli <50611440+leyuuu@users.noreply.github.com>
2021-02-22 00:03:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ rclone copy $drivepath/config.json .
 pip3 install -r requirements.txt
 python3 main.py
 
-aria2c --seed-time=0 -d downloads -i dlist
+aria2c --seed-time=0 -d downloads -i dlist.txt
 
 rclone copy downloads $drivepath
 rclone copy config.json $drivepath
